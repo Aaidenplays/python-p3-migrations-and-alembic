401dcdd25b19696f3ece1fc45f3b2e2c93c4cf56
Aaiden Witten <aaidenplays@yahoo.com>
2023-02-10 11:05:31 -0600

    wot

---
+++ lib/migrations/versions/51dede2eae5d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 51dede2eae5d
+Revises: 
+Create Date: 2023-02-10 10:47:45.042497
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51dede2eae5d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
